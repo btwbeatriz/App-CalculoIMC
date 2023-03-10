0cd1837b6baa0c1e5d7393144e53b68ec859ffaa
btwbeatriz <rm95246@fiap.com.br>
2023-03-10 18:51:11 -0300

    projeto finalizado

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,  { useState } from 'react';
+import {Alert, View, Text, TextInput, StyleSheet, Button} from 'react-native';
 
-export default function App() {
+
+export function App() {
+
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [imc, setImc] = useState('');
+
+  const calcularIMC = () => {
+    const imcCalculado = Number(peso) / (Number(altura) * Number(altura));
+    setImc(imcCalculado.toFixed(1));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ estilos.viewPrincipal }>
+      <Text style={estilos.texto}> Digite seu peso: </Text>
+      <TextInput style={estilos.texto}
+        placeholder="Peso"
+        onChangeText={setPeso}
+        value={peso}
+        keyboardType="numeric" />
+
+      <Text style={estilos.texto}> Digite sua altura: </Text>
+      <TextInput style={estilos.texto}
+        placeholder="Altura"
+        onChangeText={setAltura}
+        value={altura}
+        keyboardType="numeric" />
+        
+        <Button 
+        title="Calcular IMC"
+        color="#006DB2"
+        onPress={calcularIMC}/>
+        <Text style={estilos.viewPrincipal}>Seu IMC é: {imc}</Text>
     </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const estilos = StyleSheet.create( {
+    
+  viewPrincipal:{
+    marginTop: 60,
+    textAlign: 'center'
+  },
+  texto: {
+    textAlign: 'center',
+    
   },
 });
+
+export default App;
